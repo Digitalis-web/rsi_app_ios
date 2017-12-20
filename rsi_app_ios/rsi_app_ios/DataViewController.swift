94558c40e6da3ababc48fe1349514f8bab8661e0
Oliver Gustavsson <olivergustavsson@LearniOSFreecoms-Mac.local>
2017-12-20 13:35:20 +0000

    hej

---
+++ rsi_app_ios/rsi_app_ios/DataViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DataViewController.swift
+//  rsi_app_ios
+//
+//  Created by Oliver Gustavsson on 20/12/17.
+//  Copyright © 2017 dps. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
